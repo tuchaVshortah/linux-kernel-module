d1bdf7afc4fd1e166382908d7ca4e64b6c8af1a2
tuchaVshortah <71591558+tuchaVshortah@users.noreply.github.com>
2023-02-23 18:41:12 +0600

    Create a hello world module

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your name");
+MODULE_DESCRIPTION("A simple hello world module");
+
+static int __init hello_init(void) {
+    printk(KERN_INFO "Hello, world!\n");
+    return 0;
+}
+
+static void __exit hello_exit(void) {
+    printk(KERN_INFO "Goodbye, world!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
